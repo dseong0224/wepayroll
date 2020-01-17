c7206437c4ad0dc835fa9a0137d0446e71bc1600
Daniel Seong <dseong0224@gmail.com>
2020-01-17 02:11:45 -0800

    initial commit

---
+++ Paycompute.Services/IEmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using Paycompute.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paycompute.Services
+{
+    public interface IEmployeeService
+    {
+        Task CreateAsync(Employee newEmployee);
+        //Task is a keyword to create methods -  Task class represents a single operation that does not return a value and that usually executes asynchronously
+        Employee GetById(int employeedId);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int id);
+        Task Delete(int employeeId);
+        decimal UnionFees(int id);
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount);
+        IEnumerable<Employee> GetAll();
+    }
+}
